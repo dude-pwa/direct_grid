4801912429222972baeb3b194e82cead2ea9b634
mrpwa <dudepwa@gmail.com>
2016-10-14 09:26:35 +0700

    p2: edit grid.js add plugin RowEditing to it

--- grid.js
+++ grid.js
@@ -6,7 +6,7 @@ Ext.onReady(function() {
 		extend: 'Ext.data.Model',
 		fields: ['id', 'name', 'address', 'state']
 	});
-	
+
 	//separated store into unique var for guaranteeRange
 	var store = Ext.create('Ext.data.Store', {
 		model: 'PersonalInfo',
@@ -21,12 +21,30 @@ Ext.onReady(function() {
 			    }
 		}
 	});
+
+	var rowEditing = Ext.create('Ext.grid.plugin.RowEditing', {
+	    clicksToMoveEditor: 1,
+	    autoCancel: false
+	});
+
+	var alphaSpaceTest = /^[-\sa-zA-Z]+$/;
+	
+	Ext.apply(Ext.form.field.VTypes, {
+	    //  vtype validation function
+	    alphaSpace: function(val, field) {
+	        return alphaSpaceTest.test(val);
+	    },
+	    // vtype Text property: The error text to display when the validation function returns false
+	    alphaSpaceText: 'Not a valid state.  Must not contain numbers.',
+	    // vtype Mask property: The keystroke filter mask
+	    alphaSpaceMask: /^[-\sa-zA-Z]+$/
+	});
 	
 	//create the grid
 	var grid = Ext.create('Ext.grid.Panel', {
 		height: 450,
 		width: 700,
-		title: 'Velociraptor Owners',
+		title: 'Personal Detail',
 		store: store,
 		columns: [{
 			dataIndex: 'id',
@@ -35,16 +53,84 @@ Ext.onReady(function() {
 		}, {
 			dataIndex: 'name',
 			flex: 1,
-			text: 'Name'
+			text: 'Name',
+			// pt2
+			allowBlank: false,
+			field: {
+				type: 'textfield',
+				allowBlank: false
+			}
 		}, {
 			dataIndex: 'address',
 			flex: 1.3,
-			text: 'Address'
+			text: 'Address',
+			// pt2
+			allowBlank: false,
+			field: {
+				type: 'textfield',
+				allowBlank: false
+			}
 		}, {
 			dataIndex: 'state',
 			flex: 1,
-			text: 'State'
+			text: 'State',
+			// pt2
+			allowBlank: false,
+			field: {
+				type: 'textfield',
+				allowBlank: false,
+				vtype: 'alphaSpace'
+			}
 		}],
-		renderTo: Ext.getBody()
+		renderTo: Ext.getBody(),
+		//pt 2
+		plugins: [
+			rowEditing
+		],
+		dockedItems: [{
+			xtype: 'toolbar',
+			store: store,
+			dock: 'bottom',
+			//creating, add items
+			items: [{
+				iconCls: 'add',
+				text: 'Add',
+				handler: function() {
+					rowEditing.cancelEdit();
+					// create a record
+					var newRecord = Ext.create('PersonalInfo');
+					store.insert(0, newRecord);
+					rowEditing.startEdit(0, 0);
+					
+					// write about this section in tutorial
+					var sm = grid.getSelectionModel();
+					grid.on('edit', function() {
+						var record = sm.getSelection()
+						store.sync();
+						store.remove(record);
+						store.load();
+					});
+				}
+			}, {
+				iconCls: 'delete',
+				text: 'Delete',
+				handler: function() {
+					rowEditing.cancelEdit();
+					var sm = grid.getSelectionModel();
+					Ext.Msg.show({
+					     title:'Delete Record?',
+					     msg: 'You are deleting a record permanently, this cannot be undone. Proceed?',
+					     buttons: Ext.Msg.YESNO,
+					     icon: Ext.Msg.QUESTION,
+					     fn: function(btn){
+					     	if(btn === 'yes') {
+					     		store.remove(sm.getSelection());
+					     		store.sync();
+					     	}
+					     }
+					});
+				}
+			}]
+		}]
 	});
 });
