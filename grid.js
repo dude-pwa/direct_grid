bf138e4efddb814c9120ef8742eb5cc336388632
mrpwa <dudepwa@gmail.com>
2016-10-14 08:22:05 +0700

    membuat grid.js

---
+++ grid.js
@@ -0,0 +1,45 @@
+Ext.onReady(function() {
+	Ext.direct.Manager.addProvider(Ext.app.REMOTING_API);
+	
+	//added model inside onready
+	Ext.define('PersonalInfo', {
+		extend: 'Ext.data.Model',
+		fields: ['id', 'name', 'address', 'state']
+	});
+	
+	//separated store into unique var for guaranteeRange
+	var store = Ext.create('Ext.data.Store', {
+		model: 'PersonalInfo',
+		autoLoad: true,
+		proxy: {
+			type: 'direct',
+			directFn: QueryDatabase.getResults,
+		}
+	});
+	
+	//create the grid
+	var grid = Ext.create('Ext.grid.Panel', {
+		height: 450,
+		width: 700,
+		title: 'Velociraptor Owners',
+		store: store,
+		columns: [{
+			dataIndex: 'id',
+			width: 50,
+			text: 'ID'
+		}, {
+			dataIndex: 'name',
+			flex: 1,
+			text: 'Name'
+		}, {
+			dataIndex: 'address',
+			flex: 1.3,
+			text: 'Address'
+		}, {
+			dataIndex: 'state',
+			flex: 1,
+			text: 'State'
+		}],
+		renderTo: Ext.getBody()
+	});
+});
